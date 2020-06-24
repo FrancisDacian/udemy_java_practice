aded75572d7812d98aed0ac21bb07781de7981d9
YU RONGQIAN <2411837915@qq.com>
2020-06-25 00:07:08 +0800

    Create EqualByThreeDecimalPlaces.java

---
+++ section4/EqualByThreeDecimalPlaces.java
@@ -0,0 +1,10 @@
+public static boolean areEqualByThreeDecimalPlaces(double num1, double num2) {
+        double result1 = num1 * 1000;
+        double result2 = num2 * 1000;
+
+        if((int) result1 == (int) result2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
