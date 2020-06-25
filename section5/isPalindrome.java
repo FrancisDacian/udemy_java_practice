ab5022f7b8cb05b973cc3cb56b6847f2ba4505d5
YU RONGQIAN <2411837915@qq.com>
2020-06-25 22:17:30 +0800

    Create isPalindrome.java

---
+++ section5/isPalindrome.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(1223));
+    }
+
+    public static boolean isPalindrome(int number) {
+
+        int startNumber = number;
+        int reverseVal = 0;
+        int lastNumber;
+
+        while(number != 0) {
+
+            lastNumber = number % 10;
+            reverseVal *= 10;
+            reverseVal += lastNumber;
+            number /= 10;
+
+        }
+        return reverseVal == startNumber;
+    }
+}
